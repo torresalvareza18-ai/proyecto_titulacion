66a1549a4a143fc361d2d87d5d5b1a76aee23c32
Angel Torres <torres.alvarez.a18@gmail.com>
2025-12-16 00:35:23 -0600

    fix/bookmarks

--- proyecto_titulacion/lib/features/bookmarkPost/service/bookmark_api_service.dart
+++ proyecto_titulacion/lib/features/bookmarkPost/service/bookmark_api_service.dart
@@ -38,6 +38,7 @@ class BookmarkAPIService {
                   authorName
                   createdAt
                   updatedAt
+                  authorFamily
                 }
               }
             }
